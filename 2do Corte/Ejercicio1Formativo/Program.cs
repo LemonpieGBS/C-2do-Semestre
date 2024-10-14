0dc680c01d5f5c7ba8afc05363ded00e619bdaf6
LemonpieGBS <labc202@uam.net.ni>
2024-10-14 07:25:45 -0600

    Resolver Ejercicio1 Formativo (AreaTriangulo)

--- 2do Corte/Ejercicio1Formativo/Program.cs
+++ 2do Corte/Ejercicio1Formativo/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace KIDS_SEE_GHOSTS
+{
+    public class Program
+    {
+        public static double CalcularAreaTriangulo(double baseTriangulo, double alturaTriangulo)
+        {
+            return (baseTriangulo * alturaTriangulo) / 2;
+        }
+
+        public static void Main()
+        {
+            /*
+            Crea un programa que administre un inventario de productos.
+            Cada producto tiene un nombre, un precio y una cantidad en
+            stock. El programa debe permitir:
+            ✓ Añadir nuevos productos al inventario.
+            ✓ Actualizar la cantidad en stock de un producto
+            existente.
+            ✓ Calcular el valor total del inventario (sumando el valor
+            de cada producto: precio × cantidad en stock).
+            Requisitos:
+            ✓ Implementar la función agregar_producto (inventario,
+            nombre, precio, cantidad) para añadir productos.
+            ✓ Implementar la función actualizar_stock (inventario,
+            nombre, nueva_cantidad) para modificar la cantidad de
+            un producto.
+            ✓ Implementar la función calcular_valor_total (inventario)
+            que devuelva el valor total del inventario.
+            */
+
+            Console.WriteLine("=== Calculadora de Área de Triangulos ===");
+
+            Console.WriteLine("+: Ingrese la base del tríangulo: ");
+            double baseObtenida = double.Parse(Console.ReadLine() ?? "");
+
+            Console.WriteLine("+: Ingrese la altura del tríangulo: ");
+            double alturaObtenida = double.Parse(Console.ReadLine() ?? "");
+
+            Console.WriteLine($"El área del triangulo es: {CalcularAreaTriangulo(baseObtenida, alturaObtenida)}");
+        }
+    }
+}
